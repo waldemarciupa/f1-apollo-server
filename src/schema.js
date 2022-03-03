f3f2c8b3e80032c18c4b23c03a8a55ee99da331a
waldemarciupa <waldemarciupa90@gmail.com>
2022-03-03 12:34:48 +0100

    create apollo server and schema

---
+++ src/schema.js
@@ -0,0 +1,37 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Query {
+    "Query to get races array"
+    races: [Race!]!
+  }
+
+  type Race {
+    id: ID!
+    "The race's season"
+    season: String!
+    "The race's round"
+    round: Int!
+    "The race's name"
+    name: String!
+    "The race's location"
+    location: String!
+    "The race's country"
+    country: String!
+    "The race's official formula1 url"
+    url: String!
+    "The race's sessions"
+    sessions: Session!
+  }
+
+  "The race's session"
+  type Session {
+    fp1: String!
+    fp2: String!
+    fp3: String!
+    qualifying: String!
+    gp: String!
+  }
+`;
+
+module.exports = typeDefs;

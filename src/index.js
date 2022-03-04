1bbbee0416362a007907d18ea2ae83abc9fdc027
waldemarciupa <waldemarciupa90@gmail.com>
2022-03-04 09:52:15 +0100

    change mock to resolver

--- src/index.js
+++ src/index.js
@@ -1,31 +1,34 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 
-const mocks = {
-  Query: () => ({
-    races: () => [...new Array(1)],
-  }),
-  Race: () => ({
-    id: () => 'bahrain-grand-prix',
-    season: () => '2022',
-    round: () => '1',
-    name: () => 'Bahrain Grand Prix',
-    location: () => 'Sakhir',
-    country: () => 'Bahrain',
-    url: () => 'https://www.formula1.com/en/racing/2022/Bahrain.html',
-    sessions: () => ({
+const races = [
+  {
+    id: 'bahrain-grand-prix',
+    season: '2022',
+    round: '1',
+    name: 'Bahrain Grand Prix',
+    location: 'Sakhir',
+    country: 'Bahrain',
+    url: 'https://www.formula1.com/en/racing/2022/Bahrain.html',
+    sessions: {
       fp1: '2022-03-18T12:00:00Z',
       fp2: '2022-03-18T15:00:00Z',
       fp3: '2022-03-19T12:00:00Z',
       qualifying: '2022-03-19T15:00:00Z',
       gp: '2022-03-20T15:00:00Z',
-    }),
-  }),
+    },
+  },
+];
+
+const resolvers = {
+  Query: {
+    races: () => races,
+  },
 };
 
 const server = new ApolloServer({
   typeDefs,
-  mocks,
+  resolvers,
 });
 
 server.listen().then(() => {
